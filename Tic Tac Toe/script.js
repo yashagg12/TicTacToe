e65c4a424b5118111fd645920c3e3d4d549bb112
yashagg12 <104968837+yashagg12@users.noreply.github.com>
2022-11-27 00:02:17 +0530

    Update script.js

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -12,14 +12,33 @@ let conditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
 
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to right';
+
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    document.body.style.background = gradient;
+}
+random_bg_color();
 const ticTacToe = (element, index) => {
     element.value = currentPlayer;
     element.disabled = true;
     cells[index] = currentPlayer;
     currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
     result.innerHTML = `Player ${currentPlayer} Turn`;
-
+    random_bg_color();
     for (let i = 0; i < conditions.length; i++) {
         let condition = conditions[i];
         let a = cells[condition[0]];
